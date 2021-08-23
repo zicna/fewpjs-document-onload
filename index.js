d60999947353c7627617ab6c0af40ae3d08dd669
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-23 14:35:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('The DOM has loaded');
+});
+// console.log("This cosole.log() fires when index.js loads...")
+const text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function (){
+    text.innerHTML = "This is really cool!"
+})
+
